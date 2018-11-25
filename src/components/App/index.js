aa86200c218890abd3717d48ce6f79eaeceb2fe3
leforyou <1147724499@qq.com>
2018-11-25 19:05:07 +0800

    页面组件划分

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./../../logo.svg";
+import "./style.scss";
+
+import Header from "./../Header"; //  import Header from "../Header"    <Header />
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "./style.scss";
+
+class index extends Component {
+  render() {
+    return (
+      <div className="header">
+        <div className="header-contain">
+          <div className="header-box">
+            <p className="header-title">评价列表</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default index;
